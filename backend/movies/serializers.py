c65931cbfbf6b1ad9d7849bb043a65f6e29e3e0e
fishphobiagg <dlauddn6211@icloud.com>
2023-05-09 04:47:34 +0900

    feat:movie CRUD

---
+++ backend/movies/serializers.py
@@ -0,0 +1,10 @@
+from .models import Movie
+from rest_framework import serializers
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'  
+
+class MovieSerializer(serializers.ModelSerializer):
+    pass
