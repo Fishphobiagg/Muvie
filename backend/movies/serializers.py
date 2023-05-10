604733d4cc1a1a74507cc3001e05f91e375c06fd
fishphobiagg <dlauddn6211@icloud.com>
2023-05-10 22:46:35 +0900

    feat:movieCRUD 영화 검색 api 구현

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -1,10 +1,13 @@
 from .models import Movie
 from rest_framework import serializers
 
+
 class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = '__all__'  
+        fields = '__all__'
 
+# music model 구현 이후 구현
 class MovieSerializer(serializers.ModelSerializer):
+#     ost = serializers.StringRelatedField(many=True)
     pass
--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from . models import Movie
 from . serializers import MovieListSerializer, MovieSerializer
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classe
+from django.db.models import Q
+from rest_framework.pagination import PageNumberPagination
+# from . . musics import Music
 
+class MoviePagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = 'page'
+    page_size_query_param = 'per_page'
+    max_page_size = 100
 
 @api_view(['GET'])
 def movie(request, movie_pk):
     movie = Movie.objects.get(pk=movie_pk)
-    serializers = 0
-
+    serializers = MovieSerializer()
+    serializers.data()
+    
 def user_movie_like(request, movie_pk):
     pass
 
 def search_movie_ost(request, movie_pk):
     pass
 
+@api_view(['GET'])
 def search_movie(request, keyword):
-    movie = Movie.objects.filter()
+    movie_search_result = Movie.objects.filter(Q(content__icontains=keyword)|Q(title__icontains=keyword))
+    paginator = MoviePagination()
+    result_page = paginator.paginate_queryset(movie_search_result, request)
+    serializer = MovieListSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
