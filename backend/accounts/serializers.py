f723a2323c2f6d033050ccb6d002e4f80796e152
YeryunJung <2bites@naver.com>
2023-05-13 16:09:41 +0900

    bug: cors 에러 해결

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'nickname', 'email',]
+        fields = ['id','password', 'nickname', 'email',]
 
     def create(self, validated_data):
         user = User.objects.create_user(
--- backend/muvie/settings.py
+++ backend/muvie/settings.py
@@ -53,6 +53,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+  'http://localhost:8080',
+]
+
+CORS_ALLOWED_ALL_ORIGINS = True
+
 ROOT_URLCONF = 'muvie.urls'
 
 TEMPLATES = [
