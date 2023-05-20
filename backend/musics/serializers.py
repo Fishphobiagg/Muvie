edb8d3d1df11efe90dd33f0ef23834b0361761ac
fishphobiagg <dlauddn6211@icloud.com>
2023-05-20 19:55:51 +0900

    feat:search page 중간점검

--- backend/musics/serializers.py
+++ backend/musics/serializers.py
@@ -13,17 +13,12 @@ class ComponentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class MusicListSerializer(serializers.ModelSerializer):
-    album_cover = serializers.SerializerMethodField()
     like_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Music
         fields = ['title', 'artist', 'like_count', 'album_cover', 'uri']
-    
-    def get_album_cover(self, instance):
-        album_cover = instance.movie_ost.all()[0]
-        return {"poster_path":album_cover.poster_path}
-    
+
     def get_like_count(self, instance):
         return instance.users_like_musics.count()
     
